86ff62673b0cfde39d6d9eb5b03d12df078597e5
Sandy <lshfyy@163.com>
2023-03-30 18:09:22 +0800

    'demo'

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    copy: typeof import('./../src/bizComponents/dialogs/prompt-save copy.vue')['default']
     DialogContent: typeof import('./../src/components/dialog-content/index.vue')['default']
     IconFont: typeof import('./../src/components/icon-font/icon-font.vue')['default']
     IconFontSymbol: typeof import('./../src/components/icon-font/icon-font-symbol.vue')['default']
