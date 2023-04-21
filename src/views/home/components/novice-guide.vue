1920ac2fdc59ea39db736ad1c0ccaad1c7c9df61
Sandy <lshfyy@163.com>
2023-04-21 15:06:36 +0800

    社群反馈功能开发

--- src/views/home/components/novice-guide.vue
+++ src/views/home/components/novice-guide.vue
@@ -6,17 +6,41 @@
 
 <template>
   <div class="home-guide">
-    <n-popover trigger="hover" placement="left">
+    <n-popover
+      trigger="click"
+      :show-arrow="false"
+      placement="left-end"
+      @update:show="updatePopover"
+      class="popover-feed"
+    >
+      <template #trigger>
+        <n-popover trigger="hover" placement="left" class="popover" :disabled="popFeedBack">
+          <template #trigger>
+            <div class="icon-wrap" style="margin-bottom: 16px">
+              <IconFont name="icon-icon-shequnhefankui" class="msg" />
+            </div>
+          </template>
+          <span>社群和反馈</span>
+        </n-popover>
+      </template>
+      <div class="code-feed">
+        <img
+          src="https://moyu-chat.oss-cn-hangzhou.aliyuncs.com/o-auth/Snipaste_2023-04-21_14-07-14.png"
+          alt=""
+        />
+        <p>添加AI小助手</p>
+      </div>
+    </n-popover>
+    <n-popover trigger="hover" placement="left" class="popover">
       <template #trigger>
         <div class="icon-wrap" @click="showFirstDialog('manual')">
-          <IconFont name="icon-icon-bangzhu"/>
+          <IconFont name="icon-icon-bangzhu" />
         </div>
       </template>
       <span>帮助和资源</span>
     </n-popover>
     <!-- trigger隐藏的问题怎么解决 -->
-    <n-popconfirm :show-icon="false" ref="popConfirm" :show-arrow="false" 
-                  :content-style="sam">
+    <n-popconfirm :show-icon="false" ref="popConfirm" :show-arrow="false" :content-style="sam">
       <template #trigger>
         <span></span>
       </template>
@@ -29,88 +53,92 @@
 </template>
 
 <script setup>
-import staticConfig from '@/settings/staticConfig';
-import { useApplicationStore } from '@/store/modules/application';
-import { useBizDialog } from '@/plugins';
-import {NPopconfirm} from "naive-ui"
-import {ref, reactive} from 'vue'
-import { sendLog } from '@/utils/sls-logger/sendLog';
+  import staticConfig from '@/settings/staticConfig';
+  import { useApplicationStore } from '@/store/modules/application';
+  import { useBizDialog } from '@/plugins';
+  import { NPopconfirm } from 'naive-ui';
+  import { ref, reactive } from 'vue';
+  import { sendLog } from '@/utils/sls-logger/sendLog';
 
-const dialog = useBizDialog();
-const popConfirm = ref()
-const applicationStore = useApplicationStore();
+  const dialog = useBizDialog();
+  const popConfirm = ref();
+  const popFeedBack = ref(false);
+  const applicationStore = useApplicationStore();
 
+  // todo 但是这样改不了外层样式
+  const sam = reactive({
+    borderRadius: 8 + 'px',
+  });
+  onMounted(() => {
+    autoTrigger();
+  });
 
-// todo 但是这样改不了外层样式
-const sam = reactive({
-  borderRadius: 8 + 'px'
-})
-onMounted(() => {
-  autoTrigger()
-})
-
-// 用户第一次进入自动显示引导
-function autoTrigger() {
-  let useViewCount = Number(localStorage.getItem('userViewCount')) || 0
-  if (useViewCount === 0) {
-    showFirstDialog('auto')
-  }
-  localStorage.setItem('userViewCount', useViewCount + 1)
-}
-
-function hidePopConfirm() {
-  popConfirm.value.setShow(false)
-}
-
-// 第一步
-function showFirstDialog(triggerType) {
-  // 埋点
-  report({
-    type: triggerType == 'auto' ? 'Show' : 'Click', // 自动触发 Show
-    block: 'help_center',
-    data: triggerType == 'auto' ? 0 : 1
-  })
-
-  dialog.open(
-    'guide-popup',
-    {
-      title: '1/3',
-      class: 'guide-wrap',
-      negativeText: '稍后再看',
-      positiveText: '下一步',
-      onAfterLeave: () => {
-        report({
-          type: 'Click',
-          block: 'help_step1',
-          node: 'later'
-        })
-        popConfirm.value.setShow(true)
-      },
-      onNegativeClick: () => {
-        // 点x和稍后再看都要触发提醒弹窗
-        popConfirm.value.setShow(true)
-      },
-      handlePositiveClick: () => {
-        report({
-          type: 'Click',
-          block: 'help_step1',
-          node: 'next'
-        })
-        dialog.close('guide-popup')
-        showSecondDialog()
-      }
-    },
-    {
-      guideTitle: '创作同款小程序',
-      guideContent: '输入信息，立即生成你的专属结果。可以选择是否公开为社区构建，公开后的结果会显示在当前小程序下方',
-      guideVideo: staticConfig.homeVideo1
+  // 用户第一次进入自动显示引导
+  function autoTrigger() {
+    let useViewCount = Number(localStorage.getItem('userViewCount')) || 0;
+    if (useViewCount === 0) {
+      showFirstDialog('auto');
     }
-  )
-}
+    localStorage.setItem('userViewCount', useViewCount + 1);
+  }
 
-// 第二步
-function showSecondDialog() {
-  dialog.open(
+  const updatePopover = (show) => {
+    popFeedBack.value = show;
+  };
+
+  function hidePopConfirm() {
+    popConfirm.value.setShow(false);
+  }
+  // 第一步
+  function showFirstDialog(triggerType) {
+    // 埋点
+    report({
+      type: triggerType == 'auto' ? 'Show' : 'Click', // 自动触发 Show
+      block: 'help_center',
+      data: triggerType == 'auto' ? 0 : 1,
+    });
+
+    dialog.open(
+      'guide-popup',
+      {
+        title: '1/3',
+        class: 'guide-wrap',
+        negativeText: '稍后再看',
+        positiveText: '下一步',
+        onAfterLeave: () => {
+          report({
+            type: 'Click',
+            block: 'help_step1',
+            node: 'later',
+          });
+          popConfirm.value.setShow(true);
+        },
+        onNegativeClick: () => {
+          // 点x和稍后再看都要触发提醒弹窗
+          popConfirm.value.setShow(true);
+        },
+        handlePositiveClick: () => {
+          report({
+            type: 'Click',
+            block: 'help_step1',
+            node: 'next',
+          });
+          dialog.close('guide-popup');
+          showSecondDialog();
+        },
+      },
+      {
+        guideTitle: '创作同款小程序',
+        guideContent:
+          '输入信息，立即生成你的专属结果。可以选择是否公开为社区构建，公开后的结果会显示在当前小程序下方',
+        guideVideo: staticConfig.homeVideo1,
+      }
+    );
+  }
+
+  // 第二步
+  function showSecondDialog() {
+    dialog.open(
       'guide-popup',
       {
         title: '2/3',
@@ -121,96 +149,103 @@ function showSecondDialog() {
           report({
             type: 'Click',
             block: 'help_step2',
-            node: 'later'
-          })
-          popConfirm.value.setShow(true)
+            node: 'later',
+          });
+          popConfirm.value.setShow(true);
         },
         onNegativeClick: () => {
           report({
             type: 'Click',
             block: 'help_step2',
-            node: 'forward'
-          })
-          dialog.close('guide-popup')
-          showFirstDialog()
+            node: 'forward',
+          });
+          dialog.close('guide-popup');
+          showFirstDialog();
         },
         handlePositiveClick: () => {
           report({
             type: 'Click',
             block: 'help_step2',
-            node: 'next'
-          })
-          dialog.close('guide-popup')
-          showThirdDialog()
-        }
+            node: 'next',
+          });
+          dialog.close('guide-popup');
+          showThirdDialog();
+        },
       },
       {
         guideTitle: '创作同款小程序',
-        guideContent: '您可以直接复制此款小程序的指令，也可以在此基础上进行修改，一键生成自己的小程序',
-        guideVideo: staticConfig.homeVideo2
+        guideContent:
+          '您可以直接复制此款小程序的指令，也可以在此基础上进行修改，一键生成自己的小程序',
+        guideVideo: staticConfig.homeVideo2,
       }
-    )
-}
+    );
+  }
 
-// 第三步
-function showThirdDialog() {
-  dialog.open(
-    'guide-popup',
-    {
-      title: '3/3',
-      class: 'guide-wrap',
-      onAfterLeave: () => {
-        report({
-          type: 'Click',
-          block: 'help_step3',
-          node: '-close'
-        })
-        popConfirm.value.setShow(true)
+  // 第三步
+  function showThirdDialog() {
+    dialog.open(
+      'guide-popup',
+      {
+        title: '3/3',
+        class: 'guide-wrap',
+        onAfterLeave: () => {
+          report({
+            type: 'Click',
+            block: 'help_step3',
+            node: '-close',
+          });
+          popConfirm.value.setShow(true);
+        },
+        positiveText: '开始创作',
+        handlePositiveClick: () => {
+          report({
+            type: 'Click',
+            block: 'help_step3',
+            node: '-start',
+          });
+          dialog.close('guide-popup');
+          applicationStore.changeGuideState(1);
+        },
       },
-      positiveText: '开始创作',
-      handlePositiveClick: () => {
-        report({
-          type: 'Click',
-          block: 'help_step3',
-          node: '-start'
-        })
-        dialog.close('guide-popup')
-        applicationStore.changeGuideState(1)
+      {
+        guideTitle: '创作同款小程序',
+        guideContent: '你创建的小程序都被收录在这里啦，点击查看，一键分享给好友吧',
+        guideVideo: staticConfig.homeVideo3,
       }
-    },
-    {
-      guideTitle: '创作同款小程序',
-      guideContent: '你创建的小程序都被收录在这里啦，点击查看，一键分享给好友吧',
-      guideVideo: staticConfig.homeVideo3
-    }
-  )
-}
+    );
+  }
 
-// 帮助和资源埋点
-function report(params) {
-  sendLog({
-    action_type: params.type,
-    page: 'home',
-    block: params.block,
-    node: params.node || '',
-    data: params.data || ''
-  })
-}
+  // 帮助和资源埋点
+  function report(params) {
+    sendLog({
+      action_type: params.type,
+      page: 'home',
+      block: params.block,
+      node: params.node || '',
+      data: params.data || '',
+    });
+  }
 </script>
 
 <style lang="scss">
-.guide-popconfirm {
-  color: #5652FF;
-  cursor: pointer;
-}
+  .guide-popconfirm {
+    color: #5652ff;
+    cursor: pointer;
+  }
 </style>
 <style lang="scss" scoped>
-.home-guide {
+  .home-guide {
     position: fixed;
     right: 10px;
     bottom: 50px;
+    .msg {
+      width: 20px;
+      height: 20px;
+      display: inline-block;
+      margin-top: 2px;
+    }
     .icon-wrap {
-      cursor: pointer; 
+      cursor: pointer;
       width: 36px;
       height: 36px;
       border-radius: 50%;
@@ -218,11 +253,43 @@ function report(params) {
       text-align: center;
       background-color: #fff;
       .iconfont {
+        font-size: 20px;
         color: #202226;
         &:hover {
-          color: #5652FF;
+          color: #5652ff;
         }
       }
     }
-}
+  }
+</style>
+<style lang="scss">
+  .popover-feed {
+    padding: 10px !important;
+    margin-right: 4px !important;
+    .code-feed {
+      img {
+        width: 112px;
+        height: 112px;
+        background: #f5f5f5;
+        border-radius: 4px;
+      }
+      p {
+        padding: 0;
+        margin: 0;
+        margin-top: 4px;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 18px;
+        color: #202226;
+        text-align: center;
+      }
+    }
+  }
+  .popover.n-popover {
+    --n-text-color: #fff !important;
+    --n-color: #4f5051 !important;
+    --n-divider-color: #4f5051 !important;
+    --n-box-shadow: 0px 4px 12px rgba(16, 19, 22, 0.3) !important;
+    border-radius: 4px;
+  }
 </style>
