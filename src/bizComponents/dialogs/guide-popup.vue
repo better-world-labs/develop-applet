03179dc2c8a1da7ced3d5d7aa19a9d9f1da4ddbe
Lemon <981272675@qq.com>
2023-04-23 14:51:41 +0800

    dialog-rebuild:首页引导弹出框样式、文案修改

--- src/bizComponents/dialogs/guide-popup.vue
+++ src/bizComponents/dialogs/guide-popup.vue
@@ -4,42 +4,183 @@
  * @Description: 用于新手引导
 -->
 <template>
-    <div class="guide-popup">
-			<IconFont @click="onCancel" name="icon-icon-guanbi-xiao" />
-			<div class="guide-title">{{ props.guideTitle }}</div>
-			<div class="guide-content">{{ props.guideContent }}</div>
-			<div class="guide-video">
-				<video :src="props.guideVideo" alt="" autoplay loop></video>
-			</div>
+    <div class="guide-popup"> 
+			<!-- <IconFont @click="onCancel" name="icon-icon-guanbi-xiao" /> -->
+      <div v-for="(item, index) in steps"> 
+        <template v-if="index == step">
+          <div class="guide-label">{{ step + 1 }}/3</div>
+          <div class="guide-title" >{{ item.guideTitle }}</div>
+          <div class="guide-content">{{ item.guideContent }}</div>
+          <div class="guide-video">
+            <video :src="item.guideVideo" alt="" autoplay loop></video>
+          </div>
+          <div class="option-action">
+            <n-button v-if=item.negativeText @click="item.onNegativeClick"> 
+              {{ item.negativeText }}
+            </n-button>
+            <n-button v-if="item.positiveText" type="primary" @click="item.handlePositiveClick">
+              {{ item.positiveText }}
+            </n-button>
+          </div>
+        </template>
+      </div> 
     </div>
 </template>
-
 <script setup>
+import staticConfig from '@/settings/staticConfig';
+import { sendLog } from '@/utils/sls-logger/sendLog';
 const emit = defineEmits(['close']);
 function onCancel() {
   emit('close');
 }
-const props = defineProps(['guideTitle', 'guideContent', 'guideVideo'])
+// 帮助和资源埋点
+function report(params) {
+  sendLog({
+    action_type: params.type,
+    page: 'home',
+    block: params.block,
+    node: params.node || '',
+    data: params.data || '',
+  });
+} 
+const step = ref(0);
+const steps = ref([
+  {
+    guideTitle: '一键生成专属结果',
+    guideContent: `使用AI，回答你的各种问题。
+选择喜欢的小程序，输入对话内容，点击立即生成。`,
+    guideVideo: staticConfig.homeVideo1,
+    positiveText: "下一步",
+    negativeText: "稍后再看",
+    onAfterLeave: () => {
+      report({
+        type: 'Click',
+        block: 'help_step1',
+        node: 'later',
+      })
+    },
+    handlePositiveClick: () => {
+      report({
+        type: 'Click',
+        block: 'help_step1',
+        node: 'next',
+      });
+      step.value = 1;
+    },
+    onNegativeClick: () => {
+      report({
+        type: 'Click',
+        block: 'help_step1',
+        node: 'forward',
+      }); 
+      onCancel(); // 关闭
+    }
+  },
+  {
+    guideTitle: '0代码创作小程序·得积分',
+    guideContent: '没有自己想问的？简单三步，一键生成，赚积分来来来来~',
+    guideVideo: staticConfig.homeVideo2,
+    positiveText: "下一步",
+    negativeText: "上一步",
+    onAfterLeave: () => {
+      report({
+        type: 'Click',
+        block: 'help_step2',
+        node: 'later',
+      })
+    },
+    handlePositiveClick: () => {
+      report({
+        type: 'Click',
+        block: 'help_step2',
+        node: 'next',
+      });
+       step.value = 2;
+    },
+    onNegativeClick: () => {
+      report({
+        type: 'Click',
+        block: 'help_step2',
+        node: 'forward',
+      }); 
+       step.value = 0;
+    }
+  },
+  {
+    guideTitle: '我的小程序',
+    guideContent: '创建和收藏的小程序都被收录在这里啦，查看、编辑、一键分享好友赚积分！',
+    guideVideo: staticConfig.homeVideo3,
+    positiveText: "开始创作",
+    onAfterLeave: () => {
+      report({
+        type: 'Click',
+        block: 'help_step2',
+        node: 'later',
+      }) 
+    },
+    handlePositiveClick: () => {
+      report({
+        type: 'Click',
+        block: 'help_step3',
+        node: 'next',
+      });
+      onCancel(); // 关闭
+    }
+  }
+]); 
+
 </script>
 <style lang="scss">
+.option-action{
+  margin-top: 20px;
+  float: right;
+    .n-button{
+      width: 132px !important;
+      height: 36px !important;
+      line-height: 36px;
+      text-align: center; 
+      margin-right: 17px;
+      margin-left: 0 !important;
+      font-weight: 400;
+      font-size: 14px;
+      border-radius: 8px !important;
+      background: #FFFFFF !important;
+      --n-border: none !important;
+      --n-border-hover: none !important;
+      --n-border-focus: none !important;
+    }
+    .n-button:last-child{
+      margin-right: 0px;
+    }
+     .n-button.n-button--primary-type ,.n-button--info-type{
+      background: #5652FF !important;
+      color: #FFFFFF !important;
+    }
+    .n-button.n-button--default-type,.n-button--medium-type {
+      background: #FFFFFF !important;
+      border: 1px solid #5652FF !important;
+      color: #5652FF !important;
+    }
+}
 .guide-wrap.n-dialog {
   position: fixed;
 	right: 60px;
 	bottom: 40px;
   width: 424px;
+  .n-dialog__content{
+    margin-bottom: 0px !important;
+  } 
 } 
 </style>
 
 <style lang="scss" scoped>
 .guide-popup {
-  width: 376px; 
-  margin-top: 16px;
-  background: white;
-
+  width: 524px; 
+  margin-top: -16px;
+  background: white; 
   font-weight: 400;
   font-size: 16px;
-  line-height: 22px;
-
+  line-height: 22px; 
   color: #5B5D62;
 }
 // 图标位置
@@ -55,6 +196,13 @@ const props = defineProps(['guideTitle', 'guideContent', 'guideVideo'])
 }
 .guide-popup {  
   overflow: hidden;
+  .guide-label{
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 22px; 
+    color: #5B5D62; 
+    margin-bottom: 16px; 
+  }
 	.guide-title {
 		color: #5652FF;
 		margin-bottom: 16px;
@@ -63,16 +211,18 @@ const props = defineProps(['guideTitle', 'guideContent', 'guideVideo'])
 	}
   .guide-content {
     margin-bottom: 16px;
+    white-space: pre-wrap;
   }
 	.guide-video{ 
     padding: 0;
     border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-    height: 235px;
-
+    height: 328px; 
+   
     video {
       border-radius: 8px;
-      width: 374px;  
+      width: 522px; 
+      height:100%;
       margin: 0;
     }
   }
--- src/components/novice-guide/index.vue
+++ src/components/novice-guide/index.vue
@@ -94,131 +94,16 @@
   function hidePopConfirm() {
     popConfirm.value.setShow(false);
   }
-  // 第一步
-  function showFirstDialog(triggerType) {
-    // 埋点
-    report({
-      type: triggerType == 'auto' ? 'Show' : 'Click', // 自动触发 Show
-      block: 'help_center',
-      data: triggerType == 'auto' ? 0 : 1,
-    });
 
-    dialog.open(
+function showFirstDialog() {
+    
+     dialog.open(
       'guide-popup',
-      {
-        title: '1/3',
-        class: 'guide-wrap',
-        negativeText: '稍后再看',
-        positiveText: '下一步',
-        onAfterLeave: () => {
-          report({
-            type: 'Click',
-            block: 'help_step1',
-            node: 'later',
-          });
-          popConfirm.value.setShow(true);
-        },
-        onNegativeClick: () => {
-          // 点x和稍后再看都要触发提醒弹窗
-          popConfirm.value.setShow(true);
-        },
-        handlePositiveClick: () => {
-          report({
-            type: 'Click',
-            block: 'help_step1',
-            node: 'next',
-          });
-          dialog.close('guide-popup');
-          showSecondDialog();
-        },
-      },
-      {
-        guideTitle: '创作同款小程序',
-        guideContent:
-          '输入信息，立即生成你的专属结果。可以选择是否公开为社区构建，公开后的结果会显示在当前小程序下方',
-        guideVideo: staticConfig.homeVideo1,
+      { 
+        class: 'guide-wrap', 
       }
     );
-  }
-
-  // 第二步
-  function showSecondDialog() {
-    dialog.open(
-      'guide-popup',
-      {
-        title: '2/3',
-        class: 'guide-wrap',
-        negativeText: '上一步',
-        positiveText: '下一步',
-        onAfterLeave: () => {
-          report({
-            type: 'Click',
-            block: 'help_step2',
-            node: 'later',
-          });
-          popConfirm.value.setShow(true);
-        },
-        onNegativeClick: () => {
-          report({
-            type: 'Click',
-            block: 'help_step2',
-            node: 'forward',
-          });
-          dialog.close('guide-popup');
-          showFirstDialog();
-        },
-        handlePositiveClick: () => {
-          report({
-            type: 'Click',
-            block: 'help_step2',
-            node: 'next',
-          });
-          dialog.close('guide-popup');
-          showThirdDialog();
-        },
-      },
-      {
-        guideTitle: '创作同款小程序',
-        guideContent:
-          '您可以直接复制此款小程序的指令，也可以在此基础上进行修改，一键生成自己的小程序',
-        guideVideo: staticConfig.homeVideo2,
-      }
-    );
-  }
-
-  // 第三步
-  function showThirdDialog() {
-    dialog.open(
-      'guide-popup',
-      {
-        title: '3/3',
-        class: 'guide-wrap',
-        onAfterLeave: () => {
-          report({
-            type: 'Click',
-            block: 'help_step3',
-            node: '-close',
-          });
-          popConfirm.value.setShow(true);
-        },
-        positiveText: '开始创作',
-        handlePositiveClick: () => {
-          report({
-            type: 'Click',
-            block: 'help_step3',
-            node: '-start',
-          });
-          dialog.close('guide-popup');
-          applicationStore.changeGuideState(1);
-        },
-      },
-      {
-        guideTitle: '创作同款小程序',
-        guideContent: '你创建的小程序都被收录在这里啦，点击查看，一键分享给好友吧',
-        guideVideo: staticConfig.homeVideo3,
-      }
-    );
-  }
+  } 
 
   // 帮助和资源埋点
   function report(params) {
