54b14ffc3fed1c3f0f20b2fe22c2f1f8d237a468
SamLiu <amazing_sam@outlook.com>
2023-04-28 16:15:21 +0800

    feat: ✅对接充值提现

--- src/views/my-integral/components/balance.vue
+++ src/views/my-integral/components/balance.vue
@@ -5,44 +5,65 @@
 -->
 <template>
   <div class="integral-balance">
-    <div class="total">{{total}}</div>
+    <div class="total">{{ total }}</div>
     <div class="btn-wrap">
-        <n-button type="info">立即充值</n-button>
-        <n-button>立即提现</n-button>
+      <n-button type="info" @click="activateShop">立即充值</n-button>
+      <n-button @click="activateRealization">立即提现</n-button>
     </div>
   </div>
   <div class="integral-get">积分获取攻略</div>
   <div>
     文案文案
   </div>
+  <shop @selective="selective"></shop>
+  <realization></realization>
 </template>
 
 <script setup>
-  import { getIntegral } from '@/api/user';
+import { getIntegral } from '@/api/user';
+import shop from './components/shop/index.vue';
+import realization from './components/shop/realization.vue';
 
-  import { useUserStore } from '@/store/modules/user';
-  const total = ref(0);
-  const userStore = useUserStore();
+import { useUserStore } from '@/store/modules/user';
+const { isShop, isRealization, activateShop, activateRealization } = useNative();
 
-  onMounted(() => {
-    if (!userStore.token) goAuth();
-    // 积分总额
-    getIntegral().then(({ data }) => {
-      total.value = data.total;
-    });
+const total = ref(0);
+const userStore = useUserStore();
+
+//选购
+const selective = async (data) => {
+  activateShop(false);
+  const orderData = await putOrder(data.id);
+  dialog.open(
+    'buy-wx',
+    {
+      class: 'buy-wx-dialog',
+      title: '',
+    },
+    orderData.data
+  );
+};
+onMounted(() => {
+  if (!userStore.token) goAuth();
+  // 积分总额
+  getIntegral().then(({ data }) => {
+    total.value = data.total;
   });
+});
 </script>
 
 <style lang="scss">
- .integral-balance {
+.integral-balance {
   background-color: #FFF;
   border-radius: 8px;
   padding: 24px;
+
   .total {
     font-weight: 500;
     font-size: 32px;
     color: #5652FF;
   }
+
   .btn-wrap {
     margin-top: 24px;
   }
@@ -52,5 +73,5 @@
     color: #181d24;
     font-size: 16px;
   }
- }
+}
 </style>
