3094a676fa6fea5e596a2c4e3220a3794de9c305
Sandy <lshfyy@163.com>
2023-04-28 16:37:54 +0800

    简单调整

--- src/views/my-integral/components/balance.vue
+++ src/views/my-integral/components/balance.vue
@@ -12,66 +12,67 @@
     </div>
   </div>
   <div class="integral-get">积分获取攻略</div>
-  <div>
-    文案文案
-  </div>
+  <div>文案文案</div>
   <shop @selective="selective"></shop>
   <realization></realization>
 </template>
 
 <script setup>
-import { getIntegral } from '@/api/user';
-import shop from './shop/index.vue';
-import realization from './shop/realization.vue';
-import { useNative } from './native.ts';
-import { useUserStore } from '@/store/modules/user';
-const { isShop, isRealization, activateShop, activateRealization } = useNative();
+  import { getIntegral } from '@/api/user';
+  import shop from './shop/index.vue';
+  import realization from './shop/realization.vue';
+  import { useNative } from './native.ts';
+  import { putOrder } from '@/api/application';
+  import { useUserStore } from '@/store/modules/user';
+  import { useBizDialog } from '@/plugins';
+  const dialog = useBizDialog();
+  const { isShop, isRealization, activateShop, activateRealization } = useNative();
 
-const total = ref(0);
-const userStore = useUserStore();
+  const total = ref(0);
+  const userStore = useUserStore();
 
-//选购
-const selective = async (data) => {
-  activateShop(false);
-  const orderData = await putOrder(data.id);
-  dialog.open(
-    'buy-wx',
-    {
-      class: 'buy-wx-dialog',
-      title: '',
-    },
-    orderData.data
-  );
-};
-onMounted(() => {
-  if (!userStore.token) goAuth();
-  // 积分总额
-  getIntegral().then(({ data }) => {
-    total.value = data.total;
+  //选购
+  const selective = async (data) => {
+    activateShop(false);
+    const orderData = await putOrder(data.id);
+    dialog.open(
+      'buy-wx',
+      {
+        class: 'buy-wx-dialog',
+        title: '',
+      },
+      orderData.data
+    );
+  };
+  onMounted(() => {
+    if (!userStore.token) goAuth();
+    // 积分总额
+    getIntegral().then(({ data }) => {
+      total.value = data.total;
+    });
   });
-});
 </script>
 
 <style lang="scss">
-.integral-balance {
-  background-color: #FFF;
-  border-radius: 8px;
-  padding: 24px;
+  .integral-balance {
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 24px;
 
-  .total {
-    font-weight: 500;
-    font-size: 32px;
-    color: #5652FF;
-  }
+    .total {
+      font-weight: 500;
+      font-size: 32px;
+      color: #5652ff;
+    }
 
-  .btn-wrap {
-    margin-top: 24px;
-  }
+    .btn-wrap {
+      margin-top: 24px;
+    }
 
-  .integral-get {
-    margin-top: 40px;
-    color: #181d24;
-    font-size: 16px;
+    .integral-get {
+      margin-top: 40px;
+      color: #181d24;
+      font-size: 16px;
+    }
   }
-}
 </style>
