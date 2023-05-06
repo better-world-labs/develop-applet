c09fd5fc6c3bffb1ddb30cb8e76f6a06bd6ffc61
Sandy <lshfyy@163.com>
2023-05-06 18:19:23 +0800

    充值记录 缺省添加, 样式微调

--- src/views/my-integral/components/recharge-records/index.vue
+++ src/views/my-integral/components/recharge-records/index.vue
@@ -6,7 +6,11 @@
 <template>
   <div class="integral-detail-box">
     <div class="integral-detail">
-      <div class="item" v-for="item in dataList" :key="item.orderId">
+      <div class="no-data" v-if="!state.loading && state.dataList.length == 0">
+        <img src="@/assets/no-data.png" alt="" />
+        <p>暂无充值记录</p>
+      </div>
+      <div class="item" v-else v-for="item in state.dataList" :key="item.orderId">
         <div class="item-one-line">
           <div>
             {{ item.goods.description }}
@@ -28,14 +32,22 @@
   import { getIntegralOrders } from '@/api/user';
   const userStore = useUserStore();
   import dayjs from 'dayjs';
-  const dataList = ref([]);
+  const state = reactive({
+    dataList: [],
+    loading: false,
+  });
 
   onMounted(() => {
     if (!userStore.token) goAuth();
     // 明细
-    getIntegralOrders().then(({ data }) => {
-      dataList.value = data.list;
-    });
+    state.loading = true;
+    getIntegralOrders()
+      .then(({ data }) => {
+        state.dataList = data.list;
+      })
+      .finally(() => {
+        state.loading = false;
+      });
   });
 </script>
 
@@ -44,19 +56,41 @@
     position: absolute;
     height: calc(100% - 72px - 96px - 48px);
     width: calc(100% - 312px - 104px);
+    max-width: 660px;
+    background-color: #fff;
     box-sizing: border-box;
     overflow-x: hidden;
     overflow-y: scroll;
     border-radius: 8px;
     .integral-detail {
-      background-color: #fff;
       border-radius: 8px;
-      padding: 24px;
+      padding: 12px 20px;
+
+      .no-data {
+        display: flex;
+        flex-direction: column;
+        img {
+          width: 82px;
+          height: 82px;
+          margin: 78px auto 12px;
+        }
+        p {
+          text-align: center;
+          font-size: 14px;
+          line-height: 20px;
+          color: #abacae;
+          margin: 0;
+          padding: 0;
+        }
+      }
 
       .item {
         border-bottom: 1px solid rgba(171, 172, 174, 0.2);
         padding-bottom: 8px;
         color: #202226;
+        &:last-child {
+          border-bottom: 0 none;
+        }
 
         .item-one-line {
           display: flex;
