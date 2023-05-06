547a89216d8afd46cad3398c72981aaf187724ee
SamLiu <amazing_sam@outlook.com>
2023-05-06 10:59:14 +0800

    feat: ✅我的收藏页面渲染

--- src/views/small-program/components/collect.vue
+++ src/views/small-program/components/collect.vue
@@ -4,33 +4,39 @@
  * @Description: 我收藏的
 -->
 <template>
-  <div>2</div>
+  <default-composition v-if="applicationStore.collectAppList.length <= 0" content="暂无小程序哦~"></default-composition>
+  <n-grid :x-gap="12" cols="1 860:2 1200:3 1666:4 2000:4">
+    <n-grid-item v-for="item in applicationStore.collectAppList" :key="item.id">
+      <card :item="item" @click="toDetail(item)"></card>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <script setup>
-  import { useApplicationStore } from '@/store/modules/application';
-  import { useUserStore } from '@/store/modules/user';
-  import { useInit } from '@/hooks/useInit';
-  // import MpCard from '@/bizComponents/card'
+import { useApplicationStore } from '@/store/modules/application';
+import { useUserStore } from '@/store/modules/user';
+import { useInit } from '@/hooks/useInit';
+// import MpCard from '@/bizComponents/card'
 
-  const applicationStore = useApplicationStore();
-  const userStore = useUserStore();
+const applicationStore = useApplicationStore();
+const userStore = useUserStore();
 
-  // 去小程序详情
-  function toDetail() {
-    // 注意，在详情页点返回，需返回到我的收藏
+// 去小程序详情
+function toDetail(item) {
+  // 注意，在详情页点返回，需返回到我的收藏
 
-  }
+  if (!userStore.token) goAuth();
+  $router.push({ name: 'view-template-details', query: { uuid: item.uuid } });
+}
 
-  function init() {
-    applicationStore.getCollectAppList();
-  }
+function init() {
+  applicationStore.getCollectAppList();
+}
 
-  onMounted(() => {
-    if (!userStore.token) goAuth();
-    init();
-  });
+onMounted(() => {
+  if (!userStore.token) goAuth();
+  init();
+});
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
