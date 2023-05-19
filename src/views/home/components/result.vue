cc4488d89d3a4ac216289e8c1963d9c578d5bbb7
Lemon <981272675@qq.com>
2023-05-19 15:06:04 +0800

    修改测试bug

---
+++ src/views/home/components/result.vue
@@ -0,0 +1,216 @@
+<!--
+ * @Author: Lemon
+ * @Date: 2023-05-19 14:45:27
+ * @Description: 
+-->
+<template>
+    <div class="result-item">
+        <div class="user">
+            <div>
+                <img width="36" :src="props.result?.createdBy.avatar || '@/assets/default-user.jpg'" />
+            </div>
+            <div>{{ props.result?.createdBy.nickname }}</div>
+            <div @click="setExpansion(props.num)">
+                <icon-font :name="props.slidesPerView == 2 ? 'icon-icon-zhankaikapian' : 'icon-icon-shouqikapian'" />
+            </div>
+        </div>
+        <div class="label">
+            <span>
+                {{ props.result?.inputArgs.join('·') }}
+            </span>
+        </div>
+        <div v-if="props.slidesPerView == 2" class="content">
+            {{ props.result?.content }}
+        </div>
+        <div v-else class="content-container" v-html="marked.parse(props.result?.content)">
+        </div>
+        <div class="option">
+            <div class="count">
+                {{ props.num + 1 }}/{{ applicationStore.resultTotal }}
+            </div>
+            <div class="right">
+                <div @click="option(props.result, 1)">
+                    <icon-font-symbol :name="applicationStore.resultStateList?.get(props.result.id) == 1
+                        ? 'icon-icon-yidianzan'
+                        : 'icon-icon-dianzan'
+                        " />
+
+                    <div :class="{
+                        'active-text': applicationStore.resultStateList?.get(props.result.id) == 1,
+                    }">
+                        {{ props.result.likeTimes }}
+                    </div>
+                </div>
+                <div @click="option(props.result, -1)">
+                    <icon-font-symbol :name="applicationStore.resultStateList?.get(props.result.id) == -1
+                        ? 'icon-icon-yicai'
+                        : 'icon-icon-cai'
+                        " />
+
+                    <div :class="{
+                        'active-text': applicationStore.resultStateList?.get(props.result.id) == -1,
+                    }">
+                        {{ props.result.hateTimes }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { useApplicationStore } from '@/store/modules/application';
+import { marked } from 'marked';
+
+const applicationStore = useApplicationStore();
+const props = defineProps(['slidesPerView', 'currentIndex', 'result', 'num', 'option']);
+const emit = defineEmits(['updatePerView'])
+
+const setExpansion = (index) => {
+    if (props.slidesPerView == 2) {
+        emit('updatePerView', index, 1)
+    } else {
+        emit('updatePerView', index, 2)
+    }
+}
+</script>
+<style lang="scss" scoped>
+.result-item {
+    margin: 0 8px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #202226;
+    background: #f7f7fb;
+    border-radius: 12px;
+    padding: 16px;
+    height: 242px;
+    box-sizing: border-box;
+
+    .user {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        color: #5b5d62;
+        display: flex;
+        flex-direction: row;
+
+        img {
+            width: 24px;
+            height: 24px;
+            border-radius: 24px;
+            margin-right: 8px;
+        }
+
+        >div:last-child {
+            margin-left: auto;
+
+            .iconfont {
+                font-size: 20px;
+                color: #ABACAE;
+
+                &:hover {
+                    cursor: pointer;
+                    color: #5652FF;
+                }
+            }
+        }
+    }
+
+    .label {
+        border-radius: 4px;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 30px;
+        color: #000000;
+        margin: 12px 0;
+        height: 30px;
+
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        white-space: pre-line;
+
+        span {
+            display: inline-block;
+            background: white;
+            border-radius: 4px;
+            padding: 0 8px;
+        }
+    }
+
+    .content {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        white-space: pre-line;
+        height: 96px;
+        margin-bottom: 6px;
+
+        h1 {
+            line-height: 32px;
+        }
+    }
+
+    .content-container {
+        height: 96px;
+        margin-bottom: 6px;
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
+
+    .option {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        align-content: center;
+
+
+        .count {
+            color: #ABACAE;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 18px;
+        }
+
+        .right {
+            margin-left: auto;
+            cursor: pointer;
+            color: #5B5D62;
+            display: flex;
+            flex-direction: row;
+
+
+
+            >div {
+                background: #f7f7fb;
+                margin-right: 4px;
+                padding: 4px;
+                box-sizing: border-box;
+                display: flex;
+                flex-direction: row;
+
+                .iconfont-svg {
+                    width: 24px;
+                    height: 24px;
+                    display: flex;
+                    flex-direction: row;
+                    margin-right: 6px;
+                }
+
+                &:hover {
+                    color: #202226;
+                }
+
+                .active-text {
+                    color: #5652ff;
+                }
+            }
+        }
+    }
+}
+</style>
